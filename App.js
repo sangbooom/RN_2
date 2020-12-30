a8d4ae928cad962ca45752561b10eafc55a5a526
park sangbeom <smctkdqja@gmail.com>
2020-12-30 20:15:40 +0900

    스크롤 위치에 따라 움직이는 lottie. 그리고 pullToRefrsesh를 곁들인

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+import FruitList from './FruitList';
+
+const App = () => {
+  return (
+    <View>
+      <FruitList />
+    </View>
+  );
+};
+
+export default App;
---
+++ FruitList.js
@@ -0,0 +1,129 @@
+import React, {useState, useEffect, useRef} from 'react';
+import {View, FlatList, Text, StyleSheet, Animated, Easing} from 'react-native';
+import LottieView from 'lottie-react-native';
+
+const fruitsAnimation = require('./assets/9258-bouncing-fruits.json');
+
+const fruits = [
+  'Apple',
+  'Orange',
+  'Watermelon',
+  'Avocado',
+  'Blueberry',
+  'Coconut',
+  'Durian',
+  'Mango',
+];
+
+const refreshingHeight = 100;
+const styles = StyleSheet.create({
+  flatlist: {},
+  row: {
+    height: 100,
+    justifyContent: 'center',
+    padding: 20,
+    borderBottomWidth: 3,
+    borderBottomColor: 'black',
+    backgroundColor: 'white',
+  },
+  rowTitle: {
+    fontSize: 30,
+    fontWeight: 'bold',
+  },
+
+  lottieView: {
+    height: refreshingHeight,
+    position: 'absolute',
+    top: 5,
+    left: 0,
+    right: 0,
+  },
+});
+
+function FruitList() {
+  const [offsetY, setOffsetY] = useState(0);
+  const [isRefreshing, setIsRefreshing] = useState(false);
+  const [extraPaddingTop] = useState(new Animated.Value(0));
+
+  const lottieViewRef = useRef(null);
+
+  useEffect(() => {
+    if (isRefreshing) {
+      Animated.timing(extraPaddingTop, {
+        toValue: refreshingHeight,
+        duration: 0,
+        useNativeDriver: false,
+      }).start();
+      lottieViewRef.current.play();
+    } else {
+      Animated.timing(extraPaddingTop, {
+        toValue: 0,
+        duration: 400,
+        easing: Easing.elastic(1.3),
+        useNativeDriver: false,
+      }).start();
+    }
+  }, [isRefreshing, extraPaddingTop]);
+
+  function renderItem({item}) {
+    return (
+      <View key={item} style={styles.row}>
+        <Text style={styles.rowTitle}>{item}</Text>
+      </View>
+    );
+  }
+
+  function onScroll(event) {
+    const {nativeEvent} = event;
+    const {contentOffset} = nativeEvent;
+    const {y} = contentOffset;
+    setOffsetY(y);
+  }
+
+  function onRelease() {
+    if (offsetY >= refreshingHeight && !isRefreshing) {
+      setIsRefreshing(true);
+      setTimeout(() => {
+        setIsRefreshing(false);
+      }, 3000);
+    }
+  }
+
+  let progress = 0;
+  if (offsetY > 0 && !isRefreshing) {
+    progress = offsetY / refreshingHeight;
+  }
+
+  return (
+    <View>
+      <LottieView
+        ref={lottieViewRef}
+        style={styles.lottieView}
+        source={fruitsAnimation}
+        progress={progress}
+      />
+      <FlatList
+        data={fruits}
+        keyExtractor={(item, index) => index.toString()}
+        renderItem={renderItem}
+        style={[
+          styles.flatlist,
+          {
+            paddingTop: 20,
+          },
+        ]}
+        onScroll={onScroll}
+        onResponderRelease={onRelease}
+        ListHeaderComponent={
+          <Animated.View
+            style={{
+              paddingTop: extraPaddingTop,
+            }}
+          />
+        }
+      />
+    </View>
+  );
+}
+
+export default FruitList;
